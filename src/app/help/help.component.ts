17939c2922ec4c88d6cb1024daca94186eeea868
mirceah <mircea.hasegan@gmail.com>
2019-09-16 17:53:44 +0300

    help interpolare favorite destination

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class HelpComponent {
   @Input() data: string;
+  @Input() overlayContainerStyle: any;
   @Input() overlayContentStyle: any;
 
   isHelp = false;
